710a34568e5fc32e47a552b97363bc5fd389cb5a
fsl <fsl@163.com>
2020-04-19 15:41:19 +0800

    提交多个文件

--- src/main/java/com/example/redislock/conteoller/test1.java
+++ src/main/java/com/example/redislock/conteoller/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("dev_2.0.0测试");
         System.out.println("dev_2.0.0测试1");
+        System.out.println("dev_2.0.0测试2");
     }
 }
